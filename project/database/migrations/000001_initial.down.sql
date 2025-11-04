1c150e8e1bb541f7804a5f1a13a4a2708bfab078
Brandon Smith <bsmithcompsci@gmail.com>
2025-11-03 16:47:30 -0800

    feat(database): Initial setup
    * User Profile
    * User Authentication Methods (Discord)
    * User Sessions
    * Users' Characters
    * Characters' Inventory Items Data

---
+++ project/database/migrations/000001_initial.down.sql
@@ -0,0 +1,6 @@
+-- Drop user_authenticated_sessions table
+DROP TABLE IF EXISTS user_authenticated_sessions;
+-- Drop authentication_method_discords table
+DROP TABLE IF EXISTS authentication_method_discords;
+-- Drop users table
+DROP TABLE IF EXISTS users;
